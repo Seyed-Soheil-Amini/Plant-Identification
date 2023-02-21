553b5a12693d53d89ba114f2201ef3ecac602007
Soheil Amini <so6h6eil1382@gmail.com>
2023-02-21 17:53:50 +0330

    test base directory

--- js/headerHome.js
+++ js/headerHome.js
@@ -1,3 +1,5 @@
+const base_address = window.location;
+alert(base_address);
 class HomeHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -17,7 +19,7 @@ class HomeHeader extends HTMLElement {
             <a href="ExplorePage/index.html" class="nav-item nav-link" target="_balnk">Explore</a>
             <a href="AboutPage/index.html" class="nav-item nav-link" target="_balnk">About us</a>
             <a href="HelpPage/index.html" class="nav-item nav-link" target="_balnk">Help</a>
-            <a href="project.html" class="nav-item nav-link">FAQ</a>
+            <a href="FAQ/index.html" class="nav-item nav-link">FAQ</a>
         </div>
     </div> 
     </nav>
@@ -27,7 +29,7 @@ class HomeHeader extends HTMLElement {
 
 customElements.define("home-header", HomeHeader);
 
-class IdentifyHeader extends HTMLElement {
+class OtherHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         
@@ -42,12 +44,12 @@ class IdentifyHeader extends HTMLElement {
 
         <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="../index.html" class="nav-item nav-link" target="_balnk">Home</a>
-            <a href="#" class="nav-item nav-link" active>Identify</a>
-            <a href="../Explore/index.html" class="nav-item nav-link" target="_balnk">Explore</a>
+            <a href="/index.html" class="nav-item nav-link" target="_balnk">Home</a>
+            <a href="../IdentifyPage/index.html" class="nav-item nav-link" active>Identify</a>
+            <a href="../ExplorPage/index.html" class="nav-item nav-link" target="_balnk">Explore</a>
             <a href="../AboutPage/index.html" class="nav-item nav-link" target="_balnk">About us</a>
             <a href="../HelpPage/index.html" class="nav-item nav-link"  target="_balnk">Help</a>
-            <a href="project.html" class="nav-item nav-link">FAQ</a>
+            <a href="../FAQPage/index.html" class="nav-item nav-link">FAQ</a>
         </div>
     </div> 
     </nav>
@@ -55,64 +57,4 @@ class IdentifyHeader extends HTMLElement {
     }
 }
 
-customElements.define("identify-header", IdentifyHeader);
-
-class AboutHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-        
-<!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-  <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-      <h1 class="m-0" style="font-family:Georgia, 'Times New Roman', Times, serif, oblique;">Detector plants</h1>
-  </a>
-  <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-      <span class="navbar-toggler-icon"></span>
-  </button>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="../index.html" class="nav-item nav-link" target="_balnk">Home</a>
-            <a href="../IdentifyPage/index.html" class="nav-item nav-link" target="_balnk">Identify</a>
-            <a href="../Explore/index.html" class="nav-item nav-link" target="_balnk">Explore</a>
-            <a href="#" class="nav-item nav-link" active target="_balnk">About us</a>
-            <a href="../HelpPage/index.html" class="nav-item nav-link"  target="_balnk">Help</a>
-            <a href="project.html" class="nav-item nav-link">FAQ</a>
-        </div>
-    </div> 
-    </nav>
-  `
-    }
-}
-
-customElements.define("about-header", AboutHeader);
-
-class HelpHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-        
-<!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-  <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-      <h1 class="m-0" style="font-family:Georgia, 'Times New Roman', Times, serif, oblique;">Detector plants</h1>
-  </a>
-  <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-      <span class="navbar-toggler-icon"></span>
-  </button>
-
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="../index.html" class="nav-item nav-link" target="_balnk">Home</a>
-            <a href="../IdentifyPage/index.html" class="nav-item nav-link" target="_balnk">Identify</a>
-            <a href="../ExplorePage/index.html" class="nav-item nav-link" target="_balnk">Explore</a>
-            <a href="../AboutPage" class="nav-item nav-link" target="_balnk">About us</a>
-            <a href="#" class="nav-item nav-link" active>Help</a>
-            <a href="project.html" class="nav-item nav-link">FAQ</a>
-        </div>
-    </div> 
-    </nav>
-  `
-    }
-}
-
-customElements.define("help-header", HelpHeader);
+customElements.define("other-header", OtherHeader);

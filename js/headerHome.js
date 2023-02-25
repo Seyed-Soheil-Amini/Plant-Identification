3823a9b6d7665c049963aeb7fd32d74ecc426ad6
Soheil Amini <so6h6eil1382@gmail.com>
2023-02-25 19:06:29 +0330

    Change to correct addressing

--- js/headerHome.js
+++ js/headerHome.js
@@ -4,7 +4,7 @@ class OtherHeader extends HTMLElement {
         
 <!-- Navbar Start -->
 <!-- <base href="/" target="_blank"> -->
-<base href="/Plant-Identification/" target="_blank">
+<!-- <base href="/Plant-Identification/" target="_blank"> -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
   <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
       <h1 class="m-0" style="font-family:Georgia, 'Times New Roman', Times, serif, oblique;">Detector plants</h1>
@@ -15,12 +15,12 @@ class OtherHeader extends HTMLElement {
 
         <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="index.html" class="nav-item nav-link">Home</a>
-            <a href="IdentifyPage/index.html" class="nav-item nav-link">Identify</a>
-            <a href="ExplorPage/index.html" class="nav-item nav-link">Explore</a>
-            <a href="AboutPage/index.html" class="nav-item nav-link">About us</a>
-            <a href="HelpPage/index.html" class="nav-item nav-link">Help</a>
-            <a href="FAQPage/index.html" class="nav-item nav-link">FAQ</a>
+            <a href="/index.html" class="nav-item nav-link">Home</a>
+            <a href="/IdentifyPage/index.html" class="nav-item nav-link">Identify</a>
+            <a href="/ExplorPage/index.html" class="nav-item nav-link">Explore</a>
+            <a href="/AboutPage/index.html" class="nav-item nav-link">About us</a>
+            <a href="/HelpPage/index.html" class="nav-item nav-link">Help</a>
+            <a href="/FAQPage/index.html" class="nav-item nav-link">FAQ</a>
         </div>
     </div> 
     </nav>

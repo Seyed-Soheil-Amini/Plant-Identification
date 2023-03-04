c8953f7baaf35d00c7e2053dd8f1c94ee310e244
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2023-03-04 20:59:09 +0330

    Adding header and footer to base.html , maintaining addressing

--- templates/js/headerHome.js
+++ templates/js/headerHome.js
@@ -2,29 +2,11 @@ class OtherHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         
-<!-- Navbar Start -->
-<!-- <base href="/" target="_blank"> -->
-<base href="/template" target="_blank">
-<nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
-  <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-      <h1 class="m-0" style="font-family:Georgia, 'Times New Roman', Times, serif, oblique;">Detector plants</h1>
-  </a>
-  <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-      <span class="navbar-toggler-icon"></span>
-  </button>
 
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-        <div class="navbar-nav ms-auto p-4 p-lg-0">
-            <a href="/index.html" class="nav-item nav-link">Home</a>
-            <a href="/IdentifyPage/index.html" class="nav-item nav-link">Identify</a>
-            <a href="/ExplorePage/index.html" class="nav-item nav-link">Explore</a>
-            <a href="/AboutPage/index.html" class="nav-item nav-link">About us</a>
-            <a href="/HelpPage/index.html" class="nav-item nav-link">Help</a>
-            <a href="/FAQPage/index.html" class="nav-item nav-link">FAQ</a>
-        </div>
-    </div> 
-    </nav>
+
+
   `;
+
   }
 }
 

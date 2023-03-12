731bd9ddf2eb0253b9439ef3c2703343ed4e8ebf
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2023-03-12 12:00:46 +0330

    Updating Plant model

--- identify/models.py
+++ identify/models.py
@@ -3,11 +3,24 @@ from django.db import models
 
 class Plant(models.Model):
     name = models.CharField(max_length=50)
-    english_name = models.CharField(max_length=50)
-    scientific_name = models.CharField(max_length=50)
-    family = models.CharField(max_length=50)
-    morphology = models.TextField()
-    image = models.ImageField(upload_to='mainImages/')
+    english_name = models.CharField(max_length=50, null=True, blank=True)
+    scientific_name = models.CharField(max_length=50, null=True, blank=True)
+    family = models.CharField(max_length=50, null=True, blank=True)
+    morphology = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='mainImages/', null=True, blank=True)
+    water_need = models.CharField(max_length=50, null=True, blank=True)
+    soil_need = models.CharField(max_length=50, null=True, blank=True)
+    salinity_tolerance = models.CharField(max_length=50, null=True, blank=True)
+    temperature_tolerance = models.CharField(max_length=50, null=True, blank=True)
+    species_score = models.CharField(max_length=50, null=True, blank=True)
+    first_planting_priority = models.CharField(max_length=50, null=True, blank=True)
+    second_planting_priority = models.CharField(max_length=50, null=True, blank=True)
+    third_planting_priority = models.CharField(max_length=50, null=True, blank=True)
+    suitable_planting_location = models.CharField(max_length=50, null=True, blank=True)
+    suitable_planting_combination = models.CharField(max_length=50, null=True, blank=True)
+    specific_characteristic = models.CharField(max_length=50, null=True, blank=True)
+    more_information = models.CharField(max_length=50, blank=True, null=True)
+    references = models.CharField(max_length=50, blank=True, null=True)
 
     class Meta:
         db_table = 'plants'
@@ -16,5 +29,3 @@ class Plant(models.Model):
 
     def __str__(self):
         return self.name
-
-

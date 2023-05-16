7f8bb3bd1df1d236f329fafcabd70ae1cf5ff68e
Soheil Amini <so6h6eil1382@gmail.com>
2023-05-17 01:56:06 +0430

    add logout api

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ObtainJWTWithCookie, RefreshJWTWithCookie, VerifyJWTWithCookie
+from .views import ObtainJWTWithCookie, RefreshJWTWithCookie, VerifyJWTWithCookie, LogoutView
 
 urlpatterns = [
     path('login/', ObtainJWTWithCookie.as_view(), name="login"),
     path('refresh/token/', RefreshJWTWithCookie.as_view(), name="refresh_token"),
     path('verify/token/', VerifyJWTWithCookie.as_view(), name="verify_token"),
+    path('logout/', LogoutView.as_view(), name="logout")
 ]
--- auth_api/views.py
+++ auth_api/views.py
@@ -5,9 +5,14 @@ from rest_framework.response import Response
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .serializers import CustomTokenObtainPairSerializer
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from rest_framework_simplejwt.views import TokenVerifyView
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class ObtainJWTWithCookie(TokenObtainPairView):
@@ -37,11 +42,14 @@ class RefreshJWTWithCookie(TokenRefreshView):
         # Response.data is a forEach that saves header's data in response's object
         new_access_token = response.data.get('access')
 
+        # mainResponse = Response().setdefault('default' ,'HttpOnly')
         if new_access_token:
             # Set new access token in a cookie
             mainResponse = Response('New Token is created.')
             mainResponse.set_cookie('access_token', new_access_token, expires=timedelta(minutes=5),
                                     secure=settings.SESSION_COOKIE_SECURE, httponly=True, samesite='Lax')
+        else:
+            return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
         return super().finalize_response(request, mainResponse, *args, **kwargs)
 
 
@@ -55,3 +63,26 @@ class VerifyJWTWithCookie(TokenVerifyView):
             return access_token.strip('\"')
 
         return None
+
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        # Get the refresh token from the request's cookies
+        refresh_token = request.COOKIES.get('refresh_token')
+        access_token = request.COOKIES.get('access_token')
+        # If the refresh token is not present, return an error response
+        if not refresh_token:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # Blacklist the refresh token to invalidate it
+        # try:
+        #     token_obj = RefreshToken(refresh_token)
+        #     token_obj.blacklist()
+        # except:
+        #     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # Clear the refresh token cookie
+        response = Response(status=status.HTTP_200_OK)
+        response.delete_cookie('refresh_token')
+        response.delete_cookie('access_token')
+        return response

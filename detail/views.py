67d7093c9d7993b1a8c1dbfdd5ab2595968af818
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2023-06-29 03:42:58 +0330

    Fix update api error handling

--- detail/views.py
+++ detail/views.py
@@ -67,7 +67,7 @@ class PlantDetail(APIView):
             if not bool(serializer.errors):
                 return Response(serializer.data)
             else:
-                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+                return Response(serializer.errors)
         return Response(template_name='not_found.html', status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
